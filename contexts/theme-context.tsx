3d3464d0788704cb7622e37331c7fabae9a5287b
hustle2live <kuz.v@ukr.net>
2024-08-12 17:14:21 +0300

    feat: + added bottom tabs font styles

--- contexts/theme-context.tsx
+++ contexts/theme-context.tsx
@@ -1,19 +1,53 @@
-import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import React, { createContext, useState, useEffect, ReactNode, useMemo } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import { COLORS_LIGHT, COLORS_DARK } from '../constants';
+import { StyleSheet } from 'react-native';
+import { useFonts } from 'expo-font';
 
 type ThemeType = typeof COLORS_LIGHT | typeof COLORS_DARK;
+
+interface FontStyleType {
+   [key: string]: {
+      fontSize: number;
+      fontFamily: string;
+   };
+}
 
 interface ThemeContextProps {
    theme: ThemeType;
    toggleTheme: () => void;
+   fonts: FontStyleType | null;
 }
 
 const ThemeContext = createContext<ThemeContextProps | undefined>(undefined);
 
 const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
    const [theme, setTheme] = useState<ThemeType>(COLORS_LIGHT);
+   const [fonts, setFonts] = useState<FontStyleType | null>(null);
+
+   const [fontsLoaded, error] = useFonts({
+      'LobsterTwo-Italic': require('../assets/fonts/LobsterTwo-Italic.otf'),
+      'LobsterTwo-Regular': require('../assets/fonts/LobsterTwo-Regular.otf')
+   });
+
+   useEffect(() => {
+      if (!error) {
+         const fontStyles: FontStyleType = StyleSheet.create({
+            LobsterItalic: {
+               fontSize: 20,
+               fontFamily: 'LobsterTwo-Italic'
+            },
+            LobsterRegular: {
+               fontSize: 20,
+               fontFamily: 'LobsterTwo-Regular'
+            }
+         });
+         // loading fonts use state function ...
+         console.log('fonts loaded');
+         setFonts(fontStyles);
+      }
+   }, []);
 
    // Add logic to retrieve theme from AsyncStorage here
 
@@ -28,11 +62,12 @@ const ThemeProvider: React.FC<{ children: ReactNode }> = ({ children }) => {
    // }, []);
 
    // Add logic to toggle theme here
+
    const toggleTheme = async () => {
       theme === COLORS_LIGHT ? setTheme(COLORS_DARK) : setTheme(COLORS_LIGHT);
    };
 
-   return <ThemeContext.Provider value={{ theme, toggleTheme }}>{children}</ThemeContext.Provider>;
+   return <ThemeContext.Provider value={{ theme, toggleTheme, fonts }}>{children}</ThemeContext.Provider>;
 };
 
-export { ThemeProvider, ThemeContext };
+export { ThemeProvider, ThemeContext, type FontStyleType };
--- navigation/bottom-tabs-navigator.tsx
+++ navigation/bottom-tabs-navigator.tsx
@@ -5,6 +5,7 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { AddInspiration, Dashboard, Settings } from '../screens';
 import { Ionicons } from '@expo/vector-icons';
 import { useTheme } from '../hooks';
+import { FontStyleType } from '../contexts/theme-context';
 
 const Tabs = createBottomTabNavigator();
 
@@ -16,24 +17,32 @@ type TProps = {
       name?: any;
       primaryColor: string | undefined;
       secondaryColor: string | undefined;
+      themeFonts: FontStyleType | undefined;
    };
 };
 
 const TabButton = ({ props }: TProps) => {
    const { name, focused, primaryColor, secondaryColor } = props;
-   return <Ionicons name={name} size={16} color={focused ? primaryColor : secondaryColor} />;
+   return <Ionicons name={name} size={20} color={focused ? primaryColor : secondaryColor} />;
 };
 
 const BottomTabsNavigator = () => {
-   const colors = useTheme();
-   const tabBackground = colors?.theme.APP_BACKGROUND;
-   const primaryColor = colors?.theme.PRIMARY;
-   const secondaryColor = colors?.theme.SECONDARY;
+   const themeContext = useTheme();
+   const tabBackground = themeContext?.theme.APP_BACKGROUND;
+   const primaryColor = themeContext?.theme.PRIMARY;
+   const secondaryColor = themeContext?.theme.SECONDARY;
+   const themeFonts = themeContext?.fonts ?? undefined;
 
    return (
       <Tabs.Navigator
          screenOptions={{
-            tabBarStyle: { backgroundColor: tabBackground },
+            tabBarStyle: {
+               backgroundColor: tabBackground,
+               padding: 2,
+               margin: 0,
+               justifyContent: 'center',
+               height: 50
+            },
             tabBarActiveTintColor: primaryColor,
             tabBarInactiveTintColor: secondaryColor
          }}
@@ -42,16 +51,22 @@ const BottomTabsNavigator = () => {
             name='Dashboard'
             component={Dashboard}
             options={{
+               tabBarLabelStyle: { fontFamily: themeFonts?.LobsterRegular.fontFamily, fontSize: 13, fontWeight: 400 },
                tabBarLabelPosition: 'below-icon',
-               tabBarIcon: (props) => <TabButton props={{ name: 'home', primaryColor, secondaryColor, ...props }} />
+               tabBarIcon: (props) => (
+                  <TabButton props={{ name: 'home', primaryColor, secondaryColor, themeFonts, ...props }} />
+               )
             }}
          ></Tabs.Screen>
          <Tabs.Screen
             name='Settings'
             component={Settings}
             options={{
+               tabBarLabelStyle: { fontFamily: themeFonts?.LobsterRegular.fontFamily, fontSize: 13, fontWeight: 400 },
                tabBarLabelPosition: 'below-icon',
-               tabBarIcon: (props) => <TabButton props={{ name: 'settings', primaryColor, secondaryColor, ...props }} />
+               tabBarIcon: (props) => (
+                  <TabButton props={{ name: 'settings', primaryColor, secondaryColor, themeFonts, ...props }} />
+               )
             }}
          ></Tabs.Screen>
       </Tabs.Navigator>
