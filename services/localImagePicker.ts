8e64fde3d24e10ce48578d24b8e0d73f19c830ef
hustle2live <kuz.v@ukr.net>
2024-08-29 16:23:42 +0300

    feat: clean code image picker function

--- services/localImagePicker.ts
+++ services/localImagePicker.ts
@@ -1,15 +1,12 @@
 import * as ImagePicker from 'expo-image-picker';
 
 const pickImage = async () => {
-   // No permissions request is necessary for launching the image library
    let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       allowsEditing: true,
       aspect: [4, 3],
       quality: 1
    });
-
-   console.log(result);
 
    if (!result.canceled) {
       const image = result.assets[0].uri;
