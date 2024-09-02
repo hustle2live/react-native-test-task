51388d56e8aa020ee1c4e28ad67945117b6a721d
hustle2live <kuz.v@ukr.net>
2024-09-02 15:20:24 +0300

    fix: - modified axios funcion

--- services/getRandomQuote.ts
+++ services/getRandomQuote.ts
@@ -6,13 +6,9 @@ import { GetQuoteResponseDto } from '../types';
 const getRandomQuote = async (): Promise<GetQuoteResponseDto> => {
    try {
       const response = await axios.get<GetQuoteResponseDto>(API.QUOTE_URL, {
-         params: { lang: 'en', format: 'json', method: 'getQuote' }
-         // headers: {
-         //    // 'Access-Control-Allow-Origin': '*'
-         //    'Access-Control-Allow-Origin': true
-         // }
+         params: { lang: 'en', format: 'json', method: 'getQuote' },
+         headers: { 'Access-Control-Allow-Origin': '*' }
       });
-
       return response.data;
    } catch (error) {
       console.error('Error while fetching quote', error);
