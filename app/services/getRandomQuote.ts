ab1c4eab7ac8d073de2ef0cc54b9d41fc3783f4c
hustle2live <kuz.v@ukr.net>
2024-09-09 18:23:27 +0300

    feat: + android/expo getQuotes function moderated

--- app/services/getRandomQuote.ts
+++ app/services/getRandomQuote.ts
@@ -3,6 +3,23 @@ import axios from 'axios';
 import { API } from '../common/enums';
 import { GetQuoteResponseDto } from '../common/types';
 
+const requestApiUrl = (platform): string => {
+   let ApiURL: string = '';
+
+   switch (platform) {
+      case 'windows':
+         break;
+      case 'ios':
+         break;
+      case 'android':
+         break;
+      default:
+         ApiURL = API.QUOTE_URL;
+   }
+
+   return ApiURL;
+};
+
 const getRandomQuote = async (): Promise<GetQuoteResponseDto> => {
    try {
       const response = await axios.get<GetQuoteResponseDto>(API.QUOTE_URL, {
