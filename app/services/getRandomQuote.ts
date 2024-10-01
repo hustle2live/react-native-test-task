6ca19dcbe1082598dbdb5d3e867ed1b49a21c82c
hustle2live <kuz.v@ukr.net>
2024-10-01 13:59:02 +0300

    feat: changed API proxy url

--- app/services/getRandomQuote.ts
+++ app/services/getRandomQuote.ts
@@ -6,7 +6,7 @@ import { GetQuoteResponseDto } from '../common/types';
 const getRandomQuote = async (): Promise<GetQuoteResponseDto> => {
    try {
       const [url, proxyUrl] = [API.QUOTE_URL, API.PROXY_URL.concat(API.QUOTE_URL.slice(7))];
-      const response = await axios.get<GetQuoteResponseDto>(proxyUrl, {
+      const response = await axios.get<GetQuoteResponseDto>(url, {
          params: { lang: 'en', format: 'json', method: 'getQuote' }
       });
       return response.data;
