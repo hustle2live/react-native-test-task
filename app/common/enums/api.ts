d213843c4b15c17e875bf0baa4ec78c0a3392ff6
hustle2live <kuz.v@ukr.net>
2024-10-01 22:28:18 +0300

    feat: modify get qoutes server url

--- app/common/enums/api.ts
+++ app/common/enums/api.ts
@@ -1,4 +1,6 @@
 const API = {
+   GET_QOUTE_PATH: '/api/get-qoute',
+   GH_SERVER_URL: 'https://express-rbm1.onrender.com',
    PROXY_URL: 'https://cors-anywhere.herokuapp.com/',
    QUOTE_URL: 'https://api.forismatic.com/api/1.0/',
    IMAGE_URL: 'https://picsum.photos'
--- app/services/getRandomQuote.ts
+++ app/services/getRandomQuote.ts
@@ -5,10 +5,10 @@ import { GetQuoteResponseDto } from '../common/types';
 
 const getRandomQuote = async (): Promise<GetQuoteResponseDto> => {
    try {
-      const [url, proxyUrl] = [API.QUOTE_URL, API.PROXY_URL.concat(API.QUOTE_URL.slice(7))];
-      const response = await axios.get<GetQuoteResponseDto>(url, {
-         params: { lang: 'en', format: 'json', method: 'getQuote' }
-      });
+      const url = API.GH_SERVER_URL.concat(API.GET_QOUTE_PATH);
+
+      const response = await axios.get<GetQuoteResponseDto>(url, {});
+
       return response.data;
    } catch (error) {
       console.error('Error while fetching quote', error);
