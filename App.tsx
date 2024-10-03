ef2c69b46c3b629468ca1f0007b4f03422e08c17
hustle2live <kuz.v@ukr.net>
2024-10-03 15:22:54 +0300

    feat: added dimensions

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import { StyleSheet, View } from 'react-native';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 import { RootNavigator } from './app/components/navigation';
@@ -9,8 +10,21 @@ export default function App() {
    return (
       <SafeAreaProvider>
          <ThemeProvider>
-            <RootNavigator />
+            <View style={screenStyles.content}>
+               <RootNavigator />
+            </View>
          </ThemeProvider>
       </SafeAreaProvider>
    );
 }
+
+const screenStyles = StyleSheet.create({
+   content: {
+      maxWidth: 425,
+      maxHeight: 915,
+      margin: 'auto',
+      width: '100%',
+      height: '100%',
+      minWidth: 0
+   }
+});
